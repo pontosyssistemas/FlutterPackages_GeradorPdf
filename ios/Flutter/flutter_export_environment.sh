3672fe320e0998b86cd91653dc5da91bcf16b4fd
Elvis Lima <elvis@pontosys.com>
2025-01-24 14:08:41 -0300

    Correções

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\Flutter3"
-export "FLUTTER_APPLICATION_PATH=D:\Projetos\PontoSys\Github\VirtualStore\flutterpackages\gerador_pdf"
+export "FLUTTER_APPLICATION_PATH=D:\Projetos\PontoSys\Github\Global\FlutterPackages_GeradorPdf"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1.0.0"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
